3629827237f8b08b845702ec49731cfe48a96156
U-CORP\rmcCormack <brick@brick.codes>
2018-06-05 17:13:37 -0400

    fmt

--- src/game.rs
+++ src/game.rs
@@ -6,10 +6,15 @@ enum CardSuit {
     Clubs,
     Diamonds,
     Hearts,
-    Spades
+    Spades,
 }
 
-const SUITS: [CardSuit; 4] = [CardSuit::Clubs, CardSuit::Diamonds, CardSuit::Hearts, CardSuit::Spades];
+const SUITS: [CardSuit; 4] = [
+    CardSuit::Clubs,
+    CardSuit::Diamonds,
+    CardSuit::Hearts,
+    CardSuit::Spades,
+];
 
 #[derive(Copy, Clone, Debug, Serialize)]
 enum CardValue {
@@ -25,15 +30,29 @@ enum CardValue {
     Jack,
     Queen,
     King,
-    Ace
+    Ace,
 }
 
-const VALUES: [CardValue; 13] = [CardValue::Two, CardValue::Three, CardValue::Four, CardValue::Five, CardValue::Six, CardValue::Seven, CardValue::Eight, CardValue::Nine, CardValue::Ten, CardValue::Jack, CardValue::Queen, CardValue::King, CardValue::Ace];
+const VALUES: [CardValue; 13] = [
+    CardValue::Two,
+    CardValue::Three,
+    CardValue::Four,
+    CardValue::Five,
+    CardValue::Six,
+    CardValue::Seven,
+    CardValue::Eight,
+    CardValue::Nine,
+    CardValue::Ten,
+    CardValue::Jack,
+    CardValue::Queen,
+    CardValue::King,
+    CardValue::Ace,
+];
 
 #[derive(Copy, Clone, Debug, Serialize)]
 pub struct Card {
     value: CardValue,
-    suit: CardSuit
+    suit: CardSuit,
 }
 
 pub type CardTriplet = (Option<Card>, Option<Card>, Option<Card>);
@@ -41,7 +60,7 @@ pub type CardTriplet = (Option<Card>, Option<Card>, Option<Card>);
 #[derive(Copy, Clone, Debug, Serialize)]
 pub enum GamePhase {
     Setup,
-    Play
+    Play,
 }
 
 pub struct GameState {
@@ -56,15 +75,32 @@ pub struct GameState {
 
 impl GameState {
     pub fn new(num_players: u8) -> GameState {
-        let mut deck: Vec<Card> = VALUES.iter().cycle().take(num_players as usize * VALUES.len()).zip(SUITS.iter().cycle()).map(|(value, suit)| Card { suit: *suit, value: *value }).collect();
+        let mut deck: Vec<Card> = VALUES
+            .iter()
+            .cycle()
+            .take(num_players as usize * VALUES.len())
+            .zip(SUITS.iter().cycle())
+            .map(|(value, suit)| Card {
+                suit: *suit,
+                value: *value,
+            })
+            .collect();
         rand::thread_rng().shuffle(&mut deck);
         let mut deck = deck.into_iter();
         let mut face_down_three = Vec::with_capacity(num_players as usize);
         let mut face_up_three = Vec::with_capacity(num_players as usize);
         let mut hands = Vec::with_capacity(num_players as usize);
         for _ in 0..num_players {
-            face_down_three.push((Some(deck.next().unwrap()), Some(deck.next().unwrap()), Some(deck.next().unwrap())));
-            face_up_three.push((Some(deck.next().unwrap()), Some(deck.next().unwrap()), Some(deck.next().unwrap())));
+            face_down_three.push((
+                Some(deck.next().unwrap()),
+                Some(deck.next().unwrap()),
+                Some(deck.next().unwrap()),
+            ));
+            face_up_three.push((
+                Some(deck.next().unwrap()),
+                Some(deck.next().unwrap()),
+                Some(deck.next().unwrap()),
+            ));
             hands.push(deck.by_ref().take(6).collect());
         }
         GameState {
@@ -106,14 +142,23 @@ impl GameState {
             }
         }
         PublicGameState {
-            hands: self.hands.iter().map(|x| x.len()).collect::<Vec<_>>().into_boxed_slice(),
-            face_up_three: face_up_cards.into_iter().map(|x| x.into_boxed_slice()).collect::<Vec<_>>().into_boxed_slice(),
+            hands: self
+                .hands
+                .iter()
+                .map(|x| x.len())
+                .collect::<Vec<_>>()
+                .into_boxed_slice(),
+            face_up_three: face_up_cards
+                .into_iter()
+                .map(|x| x.into_boxed_slice())
+                .collect::<Vec<_>>()
+                .into_boxed_slice(),
             face_down_three: face_down_cards.into_boxed_slice(),
             top_card: self.pile_cards.last().cloned(),
             pile_size: self.pile_cards.len(),
             cleared_size: self.cleared_cards.len(),
             cur_phase: self.cur_phase,
-            active_player: self.active_player
+            active_player: self.active_player,
         }
     }
 }
@@ -127,7 +172,7 @@ pub struct PublicGameState {
     pile_size: usize,
     cleared_size: usize,
     cur_phase: GamePhase,
-    active_player: u8
+    active_player: u8,
 }
 
 pub struct PrivateGameState {
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,138 @@
 #![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate rand;
-extern crate snowflake;
-
+extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
-
 extern crate serde;
 extern crate serde_json;
 
 mod game;
-mod lobby;
 
-use lobby::Lobby;
-use snowflake::ProcessUniqueId;
+use game::GameState;
+use rocket::State;
+use rocket_contrib::Json;
 use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::sync::RwLock;
+use std::time::Instant;
 
-#[derive(PartialEq, Eq, Hash)]
-struct PlayerId(ProcessUniqueId);
+#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone, Copy)]
+struct PlayerId(u128);
 
-#[derive(PartialEq, Eq, Hash)]
-struct LobbyId(ProcessUniqueId);
+#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone, Copy)]
+struct LobbyId(u128);
 
-impl PlayerId {
-    fn new() -> PlayerId {
-        PlayerId(ProcessUniqueId::new())
-    }
-}
-
-impl LobbyId {
-    fn new() -> LobbyId {
-        LobbyId(ProcessUniqueId::new())
-    }
+struct Lobby {
+    players: Vec<PlayerId>,
+    game: GameState,
 }
 
 struct Player {
-    active_lobby: LobbyId,
-    name: String
+    active_lobby: Option<LobbyId>,
+    name: String,
+    last_active: Instant,
 }
 
 struct ServerState {
-    lobbies: HashMap<LobbyId, Lobby>,
-    players: HashMap<PlayerId, Player>
+    lobbies: RwLock<HashMap<LobbyId, Lobby>>,
+    players: RwLock<HashMap<PlayerId, Player>>,
 }
 
 #[derive(Deserialize)]
 struct NewUserMessage {
-    name: String
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct NewLobbyMessage {
+    player_id: PlayerId,
+    num_players: u8,
 }
 
 #[derive(Deserialize)]
 enum Message {
-    NewUser(NewUserMessage)
+    NewUser(NewUserMessage),
+    NewLobby(NewLobbyMessage),
 }
 
-fn palace_serve(req: Request<Body>) -> Response<Body> {
-    let a = unsafe { SERVER_STATE.clone().unwrap() };
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, _) => {
-            let new_game = game::GameState::new(4);
-            let pub_state = new_game.public_state();
-            let serialized = ::serde_json::to_string(&pub_state).unwrap();
-            Response::new(Body::from(serialized))
-        },
-        (&Method::POST, "/api") => {
-            // Deserialize Message with serde
-            let m: Message = serde_json::from_slice::<Message>(req.body()).unwrap();
-            Response::new("hello".into())
-        },
-        _ => {
-            let mut res = Response::new(Body::empty());
-            *res.status_mut() = StatusCode::NOT_FOUND;
-            res
+#[post("/api", format = "application/json", data = "<message>")]
+fn api(
+    server_state: State<ServerState>,
+    message: Json<Message>,
+) -> rocket::response::content::Json<String> {
+    match message.into_inner() {
+        Message::NewUser(message) => {
+            let player_id = PlayerId(rand::random());
+            server_state.players.write().unwrap().insert(
+                player_id,
+                Player {
+                    name: message.name,
+                    active_lobby: None,
+                    last_active: Instant::now(),
+                },
+            );
+            rocket::response::content::Json(serde_json::to_string(&player_id).unwrap())
+        }
+        Message::NewLobby(message) => {
+            let lobby_id = LobbyId(rand::random());
+            let mut players = server_state.players.write().unwrap();
+            let mut lobbies = server_state.lobbies.write().unwrap();
+            // TODO: assert player is actually valid
+            let player = players.get_mut(&message.player_id).unwrap();
+            if let Some(old_lobby_id) = player.active_lobby {
+                let old_lobby_remaining_players = {
+                    let old_lobby = lobbies.get_mut(&old_lobby_id).unwrap();
+                    old_lobby.players = old_lobby
+                        .players
+                        .iter()
+                        .filter(|x| *x != &message.player_id)
+                        .map(|x| *x)
+                        .collect();
+                    old_lobby.players.len()
+                };
+                if old_lobby_remaining_players == 0 {
+                    lobbies.remove(&old_lobby_id);
+                }
+            }
+            player.last_active = Instant::now();
+            player.active_lobby = Some(lobby_id);
+            lobbies.insert(
+                lobby_id,
+                Lobby {
+                    players: vec![message.player_id],
+                    game: GameState::new(message.num_players),
+                },
+            );
+            rocket::response::content::Json(serde_json::to_string(&lobby_id).unwrap())
         }
     }
 }
 
-#[post("/", data = "<message>")]
-fn api(server_state: State<T>, message: Json<Message>) -> &'static str  {
-    match message {
-        Message::NewUserMessage => {
-            "hello"
-        }
-    }
+#[get("/lobbies")]
+fn lobbies(server_state: State<ServerState>) -> Json<Vec<LobbyId>> {
+    Json(
+        server_state
+            .lobbies
+            .read()
+            .unwrap()
+            .keys()
+            .map(|x| *x)
+            .collect(),
+    )
 }
 
 fn main() {
-    // @Performance should be a concurrent hash map, FMV hashing could be good as well
-    let server_state = Arc::new(RwLock::new(ServerState {
-        lobbies: HashMap::new(),
-        players: HashMap::new(),
-    }));
+    // @Performance should be a concurrent hash map, FNV hashing could be good as well
+    let server_state = ServerState {
+        lobbies: RwLock::new(HashMap::new()),
+        players: RwLock::new(HashMap::new()),
+    };
 
-    rocket::ignite().manage(server_state).mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(server_state)
+        .mount("/", routes![api, lobbies])
+        .launch();
 }
