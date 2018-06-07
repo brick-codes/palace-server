e3f2e423bd46024ff2008e8575041e563e5838e1
Richard McCormack <brick@brick.codes>
2018-06-07 11:52:43 -0400

    add turn tracking, setup phase play

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
 use rand::{self, Rng};
 
-#[derive(Copy, Clone, Debug, Serialize)]
+const HAND_SIZE: usize = 6;
+
+#[derive(Copy, Clone, Debug, Serialize, PartialEq)]
 enum CardSuit {
     Clubs,
     Diamonds,
@@ -15,7 +17,7 @@ const SUITS: [CardSuit; 4] = [
     CardSuit::Spades,
 ];
 
-#[derive(Copy, Clone, Debug, Serialize)]
+#[derive(Copy, Clone, Debug, Serialize, PartialEq)]
 enum CardValue {
     Two,
     Three,
@@ -48,7 +50,7 @@ const VALUES: [CardValue; 13] = [
     CardValue::Ace,
 ];
 
-#[derive(Copy, Clone, Debug, Serialize)]
+#[derive(Copy, Clone, Debug, Serialize, PartialEq)]
 pub struct Card {
     value: CardValue,
     suit: CardSuit,
@@ -56,7 +58,7 @@ pub struct Card {
 
 pub type CardTriplet = (Option<Card>, Option<Card>, Option<Card>);
 
-#[derive(Copy, Clone, Debug, Serialize)]
+#[derive(Copy, Clone, Debug, Serialize, PartialEq)]
 pub enum GamePhase {
     Setup,
     Play,
@@ -65,6 +67,7 @@ pub enum GamePhase {
 #[derive(Clone, Debug)]
 pub struct GameState {
     active_player: u8,
+    num_players: u8,
     hands: Box<[Vec<Card>]>,
     face_up_three: Box<[CardTriplet]>,
     face_down_three: Box<[CardTriplet]>,
@@ -101,10 +104,11 @@ impl GameState {
                 Some(deck.next().unwrap()),
                 Some(deck.next().unwrap()),
             ));
-            hands.push(deck.by_ref().take(6).collect());
+            hands.push(deck.by_ref().take(HAND_SIZE).collect());
         }
         GameState {
             active_player: 0,
+            num_players: num_players,
             hands: hands.into_boxed_slice(),
             face_down_three: face_down_three.into_boxed_slice(),
             face_up_three: face_up_three.into_boxed_slice(),
@@ -159,6 +163,60 @@ impl GameState {
             cleared_size: self.cleared_cards.len(),
             cur_phase: self.cur_phase,
             active_player: self.active_player,
+        }
+    }
+
+    pub fn choose_three_faceup(&mut self, card_one: Card, card_two: Card, card_three: Card) -> Result<(), &'static str> {
+        // Validate phase
+        if self.cur_phase != GamePhase::Setup {
+            return Err("Can only choose three faceup cards during Setup phase");
+        }
+
+        // Combine hand + face up cards
+        let mut all_cards = self.hands[self.active_player as usize].clone();
+        let face_up_cards = self.face_up_three[self.active_player as usize];
+        all_cards.push(face_up_cards.0.unwrap());
+        all_cards.push(face_up_cards.1.unwrap());
+        all_cards.push(face_up_cards.2.unwrap());
+
+        // Calculate the new hand after removing new face up cards
+        let mut card_one_removed = false;
+        let mut card_two_removed = false;
+        let mut card_three_removed = false;
+        let mut new_hand = Vec::with_capacity(HAND_SIZE);
+        for card in all_cards {
+            if card == card_one && !card_one_removed {
+                card_one_removed = true;
+            } else if card == card_two && !card_two_removed {
+                card_two_removed = true;
+            } else if card == card_three && !card_three_removed {
+                card_three_removed = true;
+            } else {
+                new_hand.push(card);
+            }
+        }
+
+        // Make sure that all three cards put face up were removed from hand
+        if !(card_one_removed && card_two_removed && card_three_removed) {
+            return Err("Three faceup cards are not in hand / already faceup cards");
+        }
+
+        // Mutate state
+        self.face_up_three[self.active_player as usize] = (Some(card_one), Some(card_two), Some(card_three));
+        self.hands[self.active_player as usize] = new_hand;
+        self.rotate_play();
+
+        if self.active_player == 0 {
+            self.cur_phase = GamePhase::Play;
+        }
+
+        Ok(())
+    }
+
+    fn rotate_play(&mut self) {
+        self.active_player += 1;
+        if self.active_player == self.num_players {
+            self.active_player = 0;
         }
     }
 }
